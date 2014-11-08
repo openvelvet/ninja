aff43b280307f001e226496b5aff77e3cb268ae9
Omar Amiri <omar@openvelvet.com>
2014-11-08 10:06:57 -0800

    Updated homepage with bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get "about" => "pages#about"
+  get "about" => "pages#about" #about_path creates path
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
