d77bed2c34842ec920d64873f8d3008696bea9db
Omar Amiri <omar@openvelvet.com>
2014-11-09 20:25:59 -0800

    Implemented Devise for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'pages#home'
   get "about" => "pages#about" #about_path creates path
   get "contact" => "pages#contact" #about_path creates path
